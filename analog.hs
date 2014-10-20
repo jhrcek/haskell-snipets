d6b84198c143414b8a93ffaa88d3a1eb4dcddbb3
Jan Hrcek <jhrcek@redhat.com>
2014-10-20 14:51:14 +0200

    Script fo finding framework.log lines with too large a delay

---
+++ analog.hs
@@ -0,0 +1,35 @@
+-- Utility script for parsing UI test's framework.log and looking for log lines 
+-- that are followed by too large a delay.
+-- Usage: runghc analog < /path/to/framework.log
+import System.Locale (defaultTimeLocale)
+import System.Environment (getArgs)
+import Data.List (isPrefixOf)
+import Data.Time (NominalDiffTime, UTCTime)
+import Data.Time.Format (readTime)
+import Data.Time.Clock (diffUTCTime)
+
+type Line = String
+
+main = do
+ args <- getArgs
+ if null args
+   then putStrLn "Usage: runghc analog <delay> < /path/to/framework.log"
+   else interact $ unlines . linesWithDelayGreaterThan 2 . filter lineHasTime . lines
+
+lineHasTime :: Line -> Bool
+lineHasTime = isPrefixOf "["
+
+linesWithDelayGreaterThan :: Int -> [Line] -> [Line]
+linesWithDelayGreaterThan minDelay (l1:l2:rest) 
+  | l2 `timeDiff` l1 > fromIntegral minDelay = ("delay " ++ show (l2 `timeDiff` l1) ++ ": " ++ l1) : linesWithDelayGreaterThan minDelay (l2:rest) 
+  | otherwise                                = linesWithDelayGreaterThan minDelay (l2:rest)
+linesWithDelayGreaterThan _  _               = []
+
+timeDiff :: Line -> Line -> NominalDiffTime
+timeDiff line1 line2 = diffUTCTime (extractTime line1) (extractTime line2)
+
+extractTime :: Line -> UTCTime
+extractTime = readHMS . take 8 . drop 10
+
+readHMS :: String -> UTCTime
+readHMS = readTime defaultTimeLocale "%H:%M:%S"
