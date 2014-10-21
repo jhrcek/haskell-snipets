01fcc4f52ad18d633b1ca38cde6bdb2d8af14bae
Jan Hrcek <jhrcek@redhat.com>
2014-10-21 07:14:53 +0200

    Date & time examples

---
+++ datetime.hs
@@ -0,0 +1,21 @@
+import Data.Time.Clock (secondsToDiffTime, picosecondsToDiffTime, DiffTime, UTCTime)
+import Data.Time.Clock.POSIX (POSIXTime, posixSecondsToUTCTime)
+import Data.Time.Format (formatTime)
+import System.Locale (defaultTimeLocale)
+
+-- DiffTime represents duration
+t1, t2, t3 :: DiffTime
+t1 = fromIntegral 10 :: DiffTime
+t2 = fromRational 1.23 :: DiffTime
+t3 = secondsToDiffTime 20
+t4 = picosecondsToDiffTime 20
+
+-- POSIXTime is type synonym for NominalDiffTime, which is like DiffTime, but with implicit stating point
+t5 :: POSIXTime
+t5 = fromIntegral 10 :: POSIXTime --10 sec after EPOCH (1.1.1970)
+
+-- UTCTime (Coordinated Universal Time) is main type for dealing with time in Haskell
+t6 :: UTCTime
+t6 = posixSecondsToUTCTime t5
+
+fmt = formatTime defaultTimeLocale "%c" t6
