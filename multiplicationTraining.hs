1487d8a065e8807d282dbdcba6da7cace49f9a24
Jan Hrcek <jhrcek@redhat.com>
2015-04-02 16:06:18 +0200

    Simple multiplication examiner to try random number generator

---
+++ multiplicationTraining.hs
@@ -0,0 +1,26 @@
+import Control.Monad (replicateM, replicateM_)
+import System.IO (hSetBuffering, stdin, BufferMode(NoBuffering), hFlush, stdout)
+import System.Random (randomRIO)
+
+main :: IO ()
+main = do
+  hSetBuffering stdin NoBuffering
+  putStrLn "How many exercises?"
+  numAttempts <- getLine
+  replicateM_ (read numAttempts) exercise
+
+exercise :: IO ()
+exercise = do
+  [a,b] <- genAssignment
+  putStr $ show a ++ " * " ++ show b ++ " = "
+  hFlush stdout --is this necessary?
+  let resStr = show (a * b)
+  answer <- replicateM (length resStr) getChar
+  putStrLn $ if answer == resStr 
+    then "\nRight"
+    else "\nWrong, it's " ++ resStr
+  
+genAssignment :: IO [Int]
+genAssignment = replicateM 2 $ randomRIO (1,10)
+                   
+
