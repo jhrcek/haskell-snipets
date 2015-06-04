c70259e98639154454defbf82589215ec2e63a63
Jan Hrcek <jhrcek@redhat.com>
2015-06-04 09:03:39 +0200

    Util for displaying graphviz source as image

---
+++ Showdot.hs
@@ -0,0 +1,19 @@
+module Showdot (showDot) where 
+import System.Process (runCommand, waitForProcess)
+import System.IO (openTempFile, hPutStr, hClose)
+import Control.Monad (void)
+
+showDot :: String -> IO ()
+showDot dot = do
+    tmpFile <- writeToTempFile dot 
+    runCmd $ "dot " ++ tmpFile ++ " -Tpng -o a.png"
+    runCmd "showimage a.png"
+ where runCmd cmdStr = runCommand cmdStr >>= void . waitForProcess 
+
+writeToTempFile :: String -> IO FilePath
+writeToTempFile str = do
+    (tmpFile, tmpHandle) <- openTempFile "." "dot.tmp"
+    putStrLn $ "Creating temp file " ++ tmpFile
+    hPutStr tmpHandle str
+    hClose tmpHandle
+    return tmpFile
