a43092675ce2ad5849c6039ff292ece8e7df36d1
Jan Hrček <jhrcek@redhat.com>
2016-01-03 08:55:36 +0100

    Parse diary update for 2016

--- parseDiary/Analyze.hs
+++ parseDiary/Analyze.hs
@@ -8,12 +8,12 @@ type DayLine = String
 toCsvLine :: Int -> Day -> String
 toCsvLine y Day {monthNum = m, dayNum = d, wordsAdded = w, revCnt = r, xCnt = x, oCnt = o} = printf "'%02d-%02d-%d', '%d', '%d', '%d', '%d'" d m y w r x o
 
-fromRight :: Either a b -> b
-fromRight (Right r) = r
-
 main = do
   putStrLn "'date', 'words added', 'review count', 'x count', 'o count'"
-  forM_ [2013, 2014] $ \year -> do
-    dayList <-fromRight <$> getData year
-    let csvLines = map (toCsvLine year) dayList
-    mapM_ putStrLn csvLines
+  forM_ [2013, 2014, 2015] $ \year -> do
+    eitherDayList <- getData year
+    case eitherDayList of
+        Right dl -> do
+            let csvLines = map (toCsvLine year) dl
+            mapM_ putStrLn csvLines
+        Left err -> print err
