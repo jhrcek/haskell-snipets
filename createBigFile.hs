8bbb873531eae5a47b9cdb33087112b00befc4ab
Jan Hrcek <jhrcek@redhat.com>
2019-01-03 12:28:00 +0100

    Script to create a big file

---
+++ createBigFile.hs
@@ -0,0 +1,9 @@
+import qualified Data.ByteString.Builder as BU
+import qualified Data.ByteString.Lazy as B
+import System.IO
+
+main :: IO ()
+main = do
+  let chunk = BU.lazyByteString $ B.pack (replicate (2^15) 0)
+  withFile "/home/jhrcek/Tmp/big" WriteMode
+      (\h -> BU.hPutBuilder h (mconcat (replicate (2^16) chunk)))
