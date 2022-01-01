4c01e036bc43a52034a7f7f332dacf6da988f8b6
Jan Hrček <honza.hrk@gmail.com>
2022-01-01 08:27:50 +0100

    Bump to lts-18.20, format via ormolu

--- parseDiary/Main.hs
+++ parseDiary/Main.hs
@@ -1,23 +1,27 @@
 {-# LANGUAGE RecordWildCards #-}
+
 module Main where
 
-import Control.Monad (forM_)
-import Parse ( Day(..), getData )
+import Data.Foldable (for_)
+import Parse (Day (..), getData)
 import Text.Printf (printf)
 
+
 type DayLine = String
+
 
 toCsvLine :: Int -> Day -> String
 toCsvLine year Day{..} =
     printf "'%02d-%02d-%d', '%d', '%d', '%d', '%d'" dayNum monthNum year wordsAdded revCnt xCnt oCnt
 
+
 main :: IO ()
 main = do
-  putStrLn "'date', 'words added', 'review count', 'x count', 'o count'"
-  forM_ [2013 .. 2020] $ \year -> do
-    eitherDayList <- getData year
-    case eitherDayList of
-        Right dl -> do
-            let csvLines = map (toCsvLine year) dl
-            mapM_ putStrLn csvLines
-        Left err -> print err
+    putStrLn "'date', 'words added', 'review count', 'x count', 'o count'"
+    for_ [2013 .. 2021] $ \year -> do
+        eitherDayList <- getData year
+        case eitherDayList of
+            Right dl -> do
+                let csvLines = map (toCsvLine year) dl
+                mapM_ putStrLn csvLines
+            Left err -> print err
--- parseDiary/Parse.hs
+++ parseDiary/Parse.hs
@@ -1,29 +1,34 @@
-{-# LANGUAGE RecordWildCards  #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeApplications #-}
-module Parse
-    ( Day(..)
-    , getData
-    ) where
+
+module Parse (
+    Day (..),
+    getData,
+) where
 
 import Data.List (elemIndex)
-import Text.Parsec (char, count, digit, many, many1, newline, noneOf, oneOf, optional, skipMany,
-                    string, upper, (<|>))
+import Text.Parsec (char, count, digit, many, many1, newline, noneOf, oneOf, optional, skipMany, string, upper, (<|>))
 import Text.Parsec.Error (ParseError)
 import Text.Parsec.Prim (unexpected)
 import Text.Parsec.String (Parser, parseFromFile)
 
-getData :: Int -> IO (Either ParseError [Day])
-getData year = parseFromFile parseYear $ "DiaryLangs" ++ show year ++".txt"
 
-data Day = Day {monthNum, dayNum, wordsAdded, revCnt, xCnt, oCnt :: Int} deriving Show
+getData :: Int -> IO (Either ParseError [Day])
+getData year = parseFromFile parseYear $ "DiaryLangs" ++ show year ++ ".txt"
+
+
+data Day = Day {monthNum, dayNum, wordsAdded, revCnt, xCnt, oCnt :: Int} deriving (Show)
+
 
 parseYear :: Parser [Day]
 parseYear = concat <$> count 12 parseMonth
+
 
 parseMonth :: Parser [Day]
 parseMonth = do
     monthIdx <- parseMonthHeader <* newline
     many1 (parseDay monthIdx) <* newline
+
 
 -- Parse number of month ("----- LEDEN -----" -> 1)
 parseMonthHeader :: Parser Int
@@ -32,36 +37,42 @@ parseMonthHeader = do
     monthStr <- many upper
     _ <- string " -----"
     case elemIndex monthStr monthNames of
-      Just idx -> return $ idx + 1
-      Nothing  -> unexpected $ "month String: " ++ monthStr
-    where monthNames = ["LEDEN", "UNOR", "BREZEN", "DUBEN", "KVETEN", "CERVEN", "CERVENEC", "SRPEN", "ZARI", "RIJEN", "LISTOPAD", "PROSINEC"]
+        Just idx -> return $ idx + 1
+        Nothing -> unexpected $ "month String: " ++ monthStr
+  where
+    monthNames = ["LEDEN", "UNOR", "BREZEN", "DUBEN", "KVETEN", "CERVEN", "CERVENEC", "SRPEN", "ZARI", "RIJEN", "LISTOPAD", "PROSINEC"]
+
 
 parseDay :: Int -> Parser Day
 parseDay monthNum = do
     dayNum <- read @Int <$> count 2 digit
-    _ <-  string ". "
+    _ <- string ". "
     (wordsAdded, revCnt) <- languageRecord
-    _ <-  many $ char ' '
+    _ <- many $ char ' '
     (xCnt, oCnt) <- parseXXX
     optional eolComment
     _ <- newline
     return Day{..}
+
+
 --  where eolComment = char '#' >> many $ noneOf '\n'
 
 eolComment :: Parser ()
 eolComment = many (char ' ') >> char '#' >> skipMany (noneOf "\n")
 
+
 languageRecord :: Parser (Int, Int)
 languageRecord = noLang <|> didLang
   where
-    noLang = char '-' >> return (0,0)
+    noLang = char '-' >> return (0, 0)
     didLang = do
-      _ <- string "D+"
-      wordsAdded <- read <$> many1 digit
-      _ <- char '('
-      revCnt <- read <$> many1 digit
-      _ <- char ')'
-      return (wordsAdded, revCnt)
+        _ <- string "D+"
+        wordsAdded <- read <$> many1 digit
+        _ <- char '('
+        revCnt <- read <$> many1 digit
+        _ <- char ')'
+        return (wordsAdded, revCnt)
+
 
 parseXXX :: Parser (Int, Int)
 parseXXX = do
